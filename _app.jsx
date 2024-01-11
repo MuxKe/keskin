d15f6e63f11705929d2bc7e0c19c8a46ebad426f
MuxKe <154708310+MuxKe@users.noreply.github.com>
2024-01-12 01:03:21 +0300

    Add files via upload

---
+++ _app.jsx
@@ -0,0 +1,12 @@
+import { SpeedInsights } from '@vercel/speed-insights/react';
+ 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
+}
+ 
+export default MyApp;
